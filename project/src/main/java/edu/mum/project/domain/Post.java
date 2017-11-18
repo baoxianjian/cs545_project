1a6d331fafc0f9c1afaea9760c5be194f0a0580f
baoxianjian <baoxianjian@gmail.com>
2017-11-19 00:54:04 +0800

    added properties into Post domain

--- project/src/main/java/edu/mum/project/domain/Post.java
+++ project/src/main/java/edu/mum/project/domain/Post.java
@@ -1,15 +1,50 @@
 package edu.mum.project.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Post {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
+	
+	private String title;
+	
+	private String content;
+	
+	private String image;
+	
+	private String video;
+	
+	private Integer type;
+	
+	private String showTime;
+	
+	private Integer memberLimit;
+	
+	private String location;
+	
+	@ManyToOne
+	private List<User> joinedUsers;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Comment> comments;
+	
+	private Boolean deleted;
+	
+	private Integer dateline;
+	
+
+	
+	
 	
 	public Post() {};
 }
